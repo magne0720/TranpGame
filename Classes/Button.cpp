c09d278d53c01a70ae24914597500b0c8d4d5ee4
堤　俊樹 <nkc.toshiki.tsutsumi@gmail.com>
2017-02-17 12:41:11 +0900

    ボタン追加

---
+++ Classes/Button.cpp
@@ -0,0 +1,61 @@
+#include "Button.h"
+
+
+
+Button* Button::create(Vec2 buttonPos)
+{
+	Button *pRet = new Button();
+	if (pRet && pRet->init(buttonPos))
+	{
+		pRet->autorelease();
+		return pRet;
+	}
+	else
+	{
+		delete pRet;
+		pRet = nullptr;
+		return nullptr;
+	}
+};
+
+bool Button::init(Vec2 buttonPos)
+{
+	if (!Node::init())
+	{
+		return false;
+	}
+	buttonSp = Sprite::create("button.png");
+	buttonSp->setScale(2);
+	buttonSp->setPosition(Vec2(designResolutionSize / 3,designResolutionSize / 2));
+	setButtonPosition(buttonPos);
+	addChild(buttonSp, 2);
+}
+
+void Button::setButtonPosition(Vec2 pos)
+{
+	//ButtonPosition = pos;
+	//buttonSp->setPosition(ButtonPosition);
+};
+
+//Vec2 Button::getButtonPosition()
+//{
+	//return ButtonPosition;
+//}
+//void update(float delta)
+//{
+//
+//}
+bool Button::onTouchBegan(const Touch * touch, Event *unused_event)
+{
+	return true;
+};
+
+void Button::onTouchMoved(const Touch * touch, Event *unused_event)
+{
+
+};
+
+void Button::onTouchEnded(const Touch * touch, Event *unused_event)
+{
+
+};
